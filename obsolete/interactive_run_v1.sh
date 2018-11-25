0402977de79daee0d60037f7f633d0844103c0da
Jeff Harwell <jharwell@fuller.edu>
2018-11-24 23:08:35 -0800

    Fixed a bug where Kubernetes would set SPARK_MASTER_PORT to correspond to the service definition, and then Spark would pick that up expecting a port and fail with java.lang.NumberFormatException.

---
+++ obsolete/interactive_run_v1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker run -it --rm --entrypoint="" -e "spark.ui.reverseProxy=false" -e "spark.ui.reverseProxyUrl=true" -e "spark.driver.memory=2g" -e "spark.executor.memory=3g" --network sparknetwork --name spark jeffharwell/spark:2.3.2_v1 /bin/sh
